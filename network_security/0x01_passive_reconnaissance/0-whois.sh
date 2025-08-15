ce1d7e24bb72aa6bef1902786b1f69d8784c32ba
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-08-15 18:48:23 +0000

    0-whois.sh//whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-whois $1 | awk /Registrant/ && /Admin/ /Tech/{action} > $1.csv
+#whois $1 | awk /Registrant/ && /Admin/ /Tech/{action} > $1.csv
+whois $1  | awk '/^Registrant/ || /^Admin/ || /^Tech/' > $1.csv
