e65a3b432c750e599ae1767ee32577ce6b4ba128
ramazan_mustafayev <mustafayevramaza121@gmail.com>
2025-08-16 18:10:45 +0000

    0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '/^(Registrant|Admin|Tech)[^:]*:/{print substr($0,1,index($0,":")-1) "," substr($0,index($0,":")+2)}' > "$1.csv"
+whois $1  | awk '/^Registrant/ || /^Admin/ || /^Tech/' > $1.csv
