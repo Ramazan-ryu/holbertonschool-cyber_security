44cbc2e3a97f3e064b6f5e38865195800f18ff60
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-09-20 22:34:58 +0400

    xor decoder bash script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-key=$1
-shift
-for byte in "$@"; do
-  result=$((byte ^ key))
-  printf "%02x " $result
+hash=$1
+hash=${hash#"{xor}"}
+
+decoded=$(echo $hash | base64 -d 2>/dev/null)
+result=""
+for ((i=0; i<${#decoded}; i++)); do
+  byte=$(printf "%d" "'${decoded:$i:1}")
+  xor_byte=$((byte ^ 15))
+  result+=$(printf "\\$(printf "%03o" $xor_byte)")
 done
-echo ""
+
+echo -n "$result"
