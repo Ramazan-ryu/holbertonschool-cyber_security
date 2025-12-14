ecaf2d6f624b0090ee10174efd25a956869bfdc9
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-12-14 04:19:45 -0500

    Write a bash script that performs comprehensive network reconnaissance using Nmap with specific NSE scripts:
    Your script should accept a host as an arguments
    Your script should probe open ports to determine service/version information.
    Your script should enable OS detection, version detection, script scanning, and traceroute.
    Your script should sequentially execute multiple NSE scripts to detect vulnerabilities across various services:
    
    Retrieve service banners from open ports.
    Enumerate supported SSL/TLS ciphers..
    Run default scripts default defined by Nmap for basic enumeration tasks.
    Enumerate SMB (Server Message Block) domains.
    Save the output to service_enumeration_results.txt for later analysis

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap --script="banner ssl-enum-ciphers default smb-enum-domains" -oN service_enumeration_results.txt $1
+nmap --script="banner,ssl-enum-ciphers,default,smb-enum-domains" -oN service_enumeration_results.txt $1
