996a8fdbd96e512e49a73f5237f689eef1fcfa8a
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-01-17 16:13:43 -0500

    Create a Ruby script that accepts command-line arguments and prints each argument.
    Method: print_arguments

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -4,7 +4,9 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    puts "Arguments:\n\n#{ARGV.join("\n")}"
+    puts "Arguments:"
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
+    end
   end
 end
-
