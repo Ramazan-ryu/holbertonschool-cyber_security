c0b06515fc12b7bd803a0055220b33d7dd3a3400
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-01-17 16:28:01 -0500

    Write a Ruby script that makes an HTTP POST request to a specified URL with optional body parameters and prints the response.
    Method: post_request
    
    Parameters:
    
    url
    body_params

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -9,9 +9,7 @@ def post_request(url, body_params = {})
 
   request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
   request.body = body_params.to_json
-
   response = http.request(request)
-
   puts "Response status: #{response.code} #{response.message}"
   puts "\nResponse body:\n\n#{JSON.pretty_generate(JSON.parse(response.body))}"
 end
