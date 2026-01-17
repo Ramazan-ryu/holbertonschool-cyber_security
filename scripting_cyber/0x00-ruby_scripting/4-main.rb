fd0e86369b5524ff36c9e913b676b34254caa03c
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-01-17 15:42:34 -0500

    Write a Ruby function merge_json_files that merges JSON objects from one file into another.
    file :file2.json
    
    Function prototype:merge_json_files(file1_path, file2_path)

--- scripting_cyber/0x00-ruby_scripting/4-main.rb
+++ scripting_cyber/0x00-ruby_scripting/4-main.rb
@@ -2,16 +2,13 @@
 
 
 require_relative '3-read_file'
-
 require_relative '4-write_file'
 
 file1_path = 'file2.json'
-
 file2_path = 'file.json'
 
 merge_json_files(file1_path, file2_path)
 
-puts "Merged JSON written to file.json"
 
 puts "File merged successfully!"
 
--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
+
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  # Читаем оба JSON-файла
-  data1 = JSON.parse(File.read(file1_path))
-  data2 = JSON.parse(File.read(file2_path))
+  # Read arrays from files
+  array1 = File.exist?(file1_path) ? JSON.parse(File.read(file1_path)) : []
+  array2 = File.exist?(file2_path) ? JSON.parse(File.read(file2_path)) : []
 
-  # Объединяем массивы
-  merged_data = data2 + data1
+  # Merge arrays
+  merged_array = array2 + array1
 
-  # Записываем обратно во второй файл
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+  # Write back to target file
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_array))
+  end
+
+  # Print message exactly as expected
+  puts "Merged JSON written to #{file2_path}"
 end
 
