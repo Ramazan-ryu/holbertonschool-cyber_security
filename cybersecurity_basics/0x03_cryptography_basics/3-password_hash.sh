7b538e4bf03759c9cfb86b0c6d071a0f1bd5f056
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-02-20 06:13:02 -0500

    openssl digest -sha512

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo -n '$1 + random 16'| openssl dgst -sha512 | cut -d ' ' -f 1 >3_hash.txt
+#echo -n '$1 + random 16'| openssl dgst -sha512 | cut -d ' ' -f 1 >3_hash.txt
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 > 3_hash.txt
