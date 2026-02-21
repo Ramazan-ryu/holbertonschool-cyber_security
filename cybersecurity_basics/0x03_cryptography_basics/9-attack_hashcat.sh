72b98d95d7df56e93a59a8341f9c18c56a4e9c87
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-02-20 20:16:51 -0500

    hash than worldists

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 0 -a 1 $1 $2 $3  > 9-password.txt
+hashcat -m 0 -a 1 "$1" worldlist1.txt worldlist2.txt --quiet && hashcat -m 0 "$1" --show | awk -F: '{print $2}' > 9-password.txt
