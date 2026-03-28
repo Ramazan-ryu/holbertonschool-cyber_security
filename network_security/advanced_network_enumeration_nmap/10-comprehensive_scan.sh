53910d43f35a6e65a20cb60f765f20f0f8f2ab0c
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-03-28 13:17:36 -0400

    Advanced Network Enumeration with Nmap

--- network_security/advanced_network_enumeration_nmap/10-comprehensive_scan.sh
+++ network_security/advanced_network_enumeration_nmap/10-comprehensive_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sV -A --script banner,ssl-enum-ciphers,default,smb-enum-domains $1 -oN service_enumeration_results.txt
+nmap -sV -p 21,80,443 --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon $1 -oN comprehensive_scan_results.txt
