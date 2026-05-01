59bb2a60792c8729ecf5309c07b6d60b3c0fb650
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2026-05-01 06:14:23 -0400

    ruby scriptting(cyber)

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,44 +1,50 @@
 #!/usr/bin/env ruby
+
 require 'optparse'
 
 file = 'tasks.txt'
 
-# Ensure file exists
-File.write(file, '') unless File.exist?(file)
-
 OptionParser.new do |op|
-  op.banner = 'Usage: cli.rb [options]'
+  op.banner  = 'Usage: cli.rb [options]'
+
 
   op.on("-a", "--add TASK", "Add a new task") do |task|
-    File.open(file, 'a') do |f|
-      f.puts(task)
+    File.open(file, 'a') do |file|
+      file.puts(task)
     end
     puts "Task '#{task}' added."
   end
 
   op.on("-l", "--list", "List all tasks") do
-    array = File.readlines(file, chomp: true).reject(&:empty?)
+    i = 1
+    array = File.readlines(file)
     puts "Tasks:"
-    array.each_with_index do |task, i|
-      puts "#{i + 1}. #{task}"
-    end
+    array.each do |line|
+      word = line.chomp!
+      puts "#{i}. #{word}"
+      i += 1
+    end 
   end
 
-  op.on("-r", "--remove INDEX", Integer, "Remove a task by index") do |index|
-    array = File.readlines(file, chomp: true).reject(&:empty?)
-    if index < 1 || index > array.length
-      puts "Invalid task index."
-      exit 1
+
+  op.on("-r", "--remove INDEX", "Remove a task by index") do |indx|
+
+    index = indx.to_i - 1
+    array = File.readlines(file)
+    rm_task = array[index].chomp!
+    array.delete_at(index)
+    File.open(file, 'w') do |file|
+      array.each do |task|
+        file.puts(task)
+      end
     end
-    removed = array.delete_at(index - 1)
-    File.open(file, 'w') do |f|
-      array.each { |task| f.puts(task) }
-    end
-    puts "Task '#{removed}' removed."
+    puts "Task '#{rm_task}' removed."
   end
+
 
   op.on("-h", "--help", "Show help") do
     puts op
     exit
   end
+
 end.parse!
